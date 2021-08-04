79b614a27992e7967c1fdc2b8e71d6dde045bb72
Yulin WANG <905946576@qq.com>
2021-08-04 18:20:58 +0100

    create UserProfile instance

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, reverse
 from django.urls.resolvers import URLPattern
 from rango import views
+from registration.backends.simple.views import RegistrationView
 
 app_name='rango'
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return reverse('rango:register_profile')
+
 
 urlpatterns=[
     path('', views.index, name='index'),
@@ -11,5 +18,6 @@ urlpatterns=[
     path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
     path('add_category/', views.add_category,name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
-    path('restricted/', views.restricted, name='restricted'),
+    # path('restricted/', views.restricted, name='restricted'),
+    path('register_profile/', views.register_profile, name='register_profile'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.models import User, UserLike, UserView
-from rango.forms import CategoryForm, PageForm
+from rango.forms import CategoryForm, PageForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -100,6 +100,24 @@ def add_page(request, category_name_slug):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+
+@login_required
+def register_profile(request):
+    form = UserProfileForm()
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            user_profile = form.save(commit=False)
+            user_profile.user = request.user
+            user_profile.save()
+            return redirect(reverse('rango:index'))
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form}
+    return render(request, 'rango/profile_registration.html', context_dict)
 
 
 @login_required
