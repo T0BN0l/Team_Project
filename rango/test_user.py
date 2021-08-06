20cbff75b45b041e84f93a803bbbcdb04244aa88
MingyuAmy <2543462w@student.gla.ac.uk>
2021-08-06 21:50:37 +0800

    [Test] Add 3 register tests for response

--- rango/test_user.py
+++ rango/test_user.py
@@ -175,3 +175,53 @@ class RegisterFormClassTests(TestCase):
         # The following URL should be available if the UserProfile model has been registered to the admin interface.
         response = self.client.get('/admin/rango/userprofile/')
         self.assertEqual(response.status_code, 200, f"{FAILURE_HEADER}When attempting to access the UserProfile in the admin interface, we didn't get a HTTP 200 status code. Did you register the new model with the admin interface?{FAILURE_FOOTER}")
+
+    def test_registration_get_response(self):
+        """
+        Checks the GET response of the registration view.
+        There should be a form with the correct markup.
+        """
+        request = self.client.get('/accounts/register/')
+        content = request.content.decode('utf-8')
+
+        self.assertTrue('<h1 class="jumbotron-heading">Register Here</h1>' in content, f"{FAILURE_HEADER}Register header's text not found, please check it.{FAILURE_FOOTER}")
+        self.assertTrue('<button type="submit" class="btn btn-primary">Submit</button>' in content, f"{FAILURE_HEADER}There is no submit button.{FAILURE_FOOTER}")
+
+    def test_bad_registration_post_response(self):
+        """
+        Checks the POST response of the registration view.
+        What if we submit a blank form?
+        """
+        request = self.client.post('/accounts/register/')
+        content = request.content.decode('utf-8')
+
+        # it will be still in the register page if user's registeration failed.
+        self.assertTrue('<h1 class="jumbotron-heading">Register Here</h1>' in content)
+
+    def test_good_form_creation(self):
+        """
+        Tests the functionality of the forms.
+        Creates a UserProfileForm and UserForm, and attempts to save them.
+        Upon completion, we should be able to login with the details supplied.
+        """
+        user_data = {'username': 'testuser', 'password': 'test123', 'email': 'test@test.com'}
+        user_form = UserForm(data=user_data)
+
+        user_profile_data = {'website': 'http://www.bing.com', 'picture': tempfile.NamedTemporaryFile(suffix=".jpg").name}
+        user_profile_form = UserProfileForm(data=user_profile_data)
+
+        self.assertTrue(user_form.is_valid(), f"{FAILURE_HEADER}The UserForm was not valid after entering the required data. Check your implementation of UserForm, and try again.{FAILURE_FOOTER}")
+        self.assertTrue(user_profile_form.is_valid(), f"{FAILURE_HEADER}The UserProfileForm was not valid after entering the required data. Check your implementation of UserProfileForm, and try again.{FAILURE_FOOTER}")
+
+        user_object = user_form.save()
+        user_object.set_password(user_data['password'])
+        user_object.save()
+
+        user_profile_object = user_profile_form.save(commit=False)
+        user_profile_object.user = user_object
+        user_profile_object.save()
+
+        self.assertEqual(len(User.objects.all()), 1, f"{FAILURE_HEADER}User was not created. Please check it.{FAILURE_FOOTER}")
+        self.assertEqual(len(UserProfile.objects.all()), 1, f"{FAILURE_HEADER}UserProfile was not created. Please check it.{FAILURE_FOOTER}")
+        self.assertTrue(self.client.login(username='testuser', password='test123'), f"{FAILURE_HEADER}Login failed with created user, please check it.{FAILURE_FOOTER}")
+
